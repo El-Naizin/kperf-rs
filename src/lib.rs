0f25f808228920cae70a1939a77959ecb01bd94e
Leo Naizin <lnaizin@gmail.com>
2023-11-11 00:37:22 +0100

    Not sure if wrapper functions should be in this crate or not :/

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,26 @@ pub mod constants;
 
 
 
-pub fn cpu_string() -> Option<Vec<u8>> {
-    unsafe {
-        let buf_size: size_t = 256;
-        let mut buf = Vec::with_capacity(buf_size as usize);
-        let buf_ptr = buf.as_mut_ptr();
-
-        let res_len = kpc_cpu_string(buf_ptr, buf_size);
-        if res_len > 0 {
-            buf.set_len(res_len as usize);
-            Some(buf)
-        } else {
-            None
-        }
-    }
-}
+// pub fn cpu_string() -> Option<Vec<u8>> {
+//     unsafe {
+//         let buf_size: size_t = 256;
+//         let mut buf = Vec::with_capacity(buf_size as usize);
+//         let buf_ptr = buf.as_mut_ptr();
+//
+//         let res_len = kpc_cpu_string(buf_ptr, buf_size);
+//         if res_len > 0 {
+//             buf.set_len(res_len as usize);
+//             Some(buf)
+//         } else {
+//             None
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use crate::{structs::*, constants::*, functions::* };
     use std::ptr::{null, null_mut};
-    use super::*;
     const LIBRARY_PATH: &str = "/System/Library/PrivateFrameworks/kperf.framework/kperf";
 
 
@@ -43,16 +42,16 @@ mod tests {
         assert_ne!(pmu_version, 0, "A PMU version of zero means an error, try to run this program in sudo");
     }
 
-    #[test]
-    fn test_get_cpu_string() {
-        let cpu_version = cpu_string();
-        // print!("CPU version: ");
-        // for c in cpu_version.unwrap() {
-        //     print!("{}", c as char);
-        // }
-        assert_ne!(cpu_version, None, "CPU Version identifier cannot be null");
-        assert_ne!(cpu_version.unwrap().len(), 0, "CPU Version identifier string cannot be of size 0");
-    }
+    // #[test]
+    // fn test_get_cpu_string() {
+    //     let cpu_version = cpu_string();
+    //     // print!("CPU version: ");
+    //     // for c in cpu_version.unwrap() {
+    //     //     print!("{}", c as char);
+    //     // }
+    //     assert_ne!(cpu_version, None, "CPU Version identifier cannot be null");
+    //     assert_ne!(cpu_version.unwrap().len(), 0, "CPU Version identifier string cannot be of size 0");
+    // }
 
     #[test]
     fn test_create_kpep_db() {
